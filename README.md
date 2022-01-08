69f88c708fa92125f6ddf147cd5481fb0bf4dfe9
wjdals7972 <wjdals6056@naver.com>
2022-01-08 23:37:15 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+Bridge BBCC
+===
+version 0.5.5
+
+
+
+## Usage
+To compile browserified.js, use that on comandline
+(node, npm, and browserify required)
+```
+npm start
+```
+If you don't want to modify source files, just use released version.
+
+readme.txt 참조
+윈도우, XSplit 외의 환경에서 동작이 보증되지 않음
+
+
+## License
+MIT
+
+## Reference
+[JSAssist Open DCcon](https://github.com/rishubil/jsassist-open-dccon)
+By [rishubil](https://github.com/rishubil) MIT License.
+
+[Twitch Developer Documentation](https://dev.twitch.tv/docs)
---
+++ source/twip_apply.js
@@ -0,0 +1,27 @@
+﻿var twipMsg =
+  configData&&configData.twipCheersMsg?
+  configData.twipCheersMsg:
+  configData.cheersMsg;
+
+exports.apply = function(message, data) {
+  if (data.nick == "twipkr") {
+    if ( (data.message.indexOf("ACTION")!=-1) && (data.message.indexOf("\\u0001")!=-1) ) {
+
+      var amount = message.match(/ (\d+,)*\d{3}/g);
+      var twipMessage = "";
+      if  (amount == null) {
+        twipMessage = twipMsg.replace("{bits}", "").
+          replace(/\{0:([^\}]*)}/g, "$1").replace(/\{!0:([^\}]*)}/g, "");
+      }
+      else {
+        amount = amount[0].split(" ")[1].replace(/[^\d]/g, "");
+        twipMessage = twipMsg.replace("{bits}", amount).replace(/((\d)\s+)?비트/, "$2원").
+          replace(/\{!0:([^\}]*)}/g, "$1").replace(/\{0:([^\}]*)}/g, "");
+      }
+
+      message = '<div class="chat_cheer_box">' + twipMessage + "</div>" + message;
+    }
+  }
+
+  return message;
+};
